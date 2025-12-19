81749c8cdac15b90821c1c1e558305b343356b1f
GregoryLacroix <gregorylacroix78@gmail.com>
2025-12-19 16:16:48 +0100

    develop livre

--- index.php
+++ index.php
@@ -1,9 +1,109 @@
 <?php
 require_once('include/_init.php');
 
+if (isset($_GET['action']) && $_GET['action'] === 'delete') {
+    if (isset($_GET['id']) && !empty($_GET['id'])) {
+        $id = $_GET['id'];
+
+        // On selectionne tout dans la table emprunt en fonction de l'id de l'abonne dans l'url, celui que l'on souhaite supprimé
+        $data = $connect_db->prepare("DELETE FROM emprunt WHERE id_emprunt = :id_emprunt");
+        $data->bindValue(':id_emprunt', $id, PDO::PARAM_INT);
+        $data->execute();
+
+        $_SESSION['msgValidate'] = "L'emprunt a été supprimé";
+
+        header('location: index.php');
+        exit;
+    }
+}
+
+if (isset($_GET['action']) && $_GET['action'] === 'update') {
+    if (isset($_GET['id']) && !empty($_GET['id'])) {
+        $id = $_GET['id'];
+        $data = $connect_db->prepare("SELECT * FROM emprunt WHERE id_emprunt = :id_emprunt");
+        $data->bindValue(':id_emprunt', $id, PDO::PARAM_INT);
+        $data->execute();
+
+        // echo "Nombre de résultat : " . $data->rowCount();
+        if (!$data->rowCount()) {
+            header('location: index.php');
+            exit;
+        }
+
+        $arrayUpdateEmprunt = $data->fetch(PDO::FETCH_ASSOC);
+        // echo '<pre>';
+        // print_r($arrayUpdateEmprunt);
+        // echo '</pre>';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $idAbonne = $_POST['abonne'];
+            $idLivre = $_POST['livre'];
+            $dateSortie = $_POST['date_sortie'];
+            $dateRendu = $_POST['date_rendu'];
+
+            if (empty($dateRendu)) {
+                $dateRendu = null;
+            }
+
+            $pdoStatement = $connect_db->prepare("UPDATE emprunt SET livre_id = :livre_id, abonne_id = :abonne_id, date_sortie = :date_sortie, date_rendu = :date_rendu WHERE id_emprunt = :id_emprunt");
+            $pdoStatement->bindValue(':livre_id', $idLivre, PDO::PARAM_INT);
+            $pdoStatement->bindValue(':abonne_id', $idAbonne, PDO::PARAM_INT);
+            $pdoStatement->bindValue(':date_sortie', $dateSortie, PDO::PARAM_STR);
+            $pdoStatement->bindValue(':date_rendu', $dateRendu, PDO::PARAM_STR);
+            $pdoStatement->bindValue(':id_emprunt', $id, PDO::PARAM_INT);
+            $pdoStatement->execute();
+
+            $_SESSION['msgValidate'] = "L'emprunt a été modifié";
+
+            header('location: index.php');
+            exit;
+        }
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $borderDanger = "border border-danger";
+
+    $idAbonne = $_POST['abonne'];
+    $idLivre = $_POST['livre'];
+    $dateSortie = $_POST['date_sortie'];
+    $dateRendu = $_POST['date_rendu'];
+
+    if (empty($idAbonne)) {
+        $error = true;
+        $msgErrorIdAbonne = "Merci de sélectionner un abonné";
+    }
+
+    if (empty($idLivre)) {
+        $error = true;
+        $msgErrorIdLivre = "Merci de sélectionner un livre";
+    }
+
+    if (empty($dateSortie)) {
+        $error = true;
+        $msgErrorDateSortie = "Merci de saisir une date";
+    }
+
+    if (empty($dateRendu)) {
+        $dateRendu = null;
+    }
+
+    if (!isset($error)) {
+        $pdoStatement = $connect_db->prepare("INSERT INTO emprunt (abonne_id, livre_id, date_sortie, date_rendu) VALUE (:abonne_id, :livre_id, :date_sortie, :date_rendu)");
+        $pdoStatement->bindValue(':abonne_id', $idAbonne, PDO::PARAM_INT);
+        $pdoStatement->bindValue(':livre_id', $idLivre, PDO::PARAM_INT);
+        $pdoStatement->bindValue(':date_sortie', $dateSortie, PDO::PARAM_STR);
+        $pdoStatement->bindValue(':date_rendu', $dateRendu, PDO::PARAM_STR);
+        $pdoStatement->execute();
+
+        $_SESSION['msgValidate'] = "L'emprunt a été enregistré";
+
+        header('location: index.php');
+        exit;
+    }
+}
+
 // Selection des emprunt
 $pdoStatement = $connect_db->query(
-"
+    "
 SELECT emprunt.id_emprunt, livre.auteur, livre.titre, abonne.prenom, emprunt.date_sortie, emprunt.date_rendu
 FROM abonne
 INNER JOIN emprunt 
@@ -28,56 +128,14 @@ $dataSelectFormLivre = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
 // print_r($_POST);
 // echo '</pre>';
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $borderDanger = "border border-danger";
-
-    $idAbonne = $_POST['abonne'];
-    $idLivre = $_POST['livre'];
-    $dateSortie = $_POST['date_sortie'];
-    $dateRendu = $_POST['date_rendu'];
-
-    if(empty($idAbonne)){
-        $error = true;
-        $msgErrorIdAbonne = "Merci de sélectionner un abonné";
-    }
-
-    if(empty($idLivre)){
-        $error = true;
-        $msgErrorIdLivre = "Merci de sélectionner un livre";
-    }
-
-    if(empty($dateSortie)){
-        $error = true;
-        $msgErrorDateSortie = "Merci de saisir une date";
-    }
-
-    if(empty($dateRendu)){
-        $dateRendu = null;
-    }
-
-    if(!isset($error)){
-        $pdoStatement = $connect_db->prepare("INSERT INTO emprunt (abonne_id, livre_id, date_sortie, date_rendu) VALUE (:abonne_id, :livre_id, :date_sortie, :date_rendu)");
-        $pdoStatement->bindValue(':abonne_id', $idAbonne, PDO::PARAM_INT);
-        $pdoStatement->bindValue(':livre_id', $idLivre, PDO::PARAM_INT);
-        $pdoStatement->bindValue(':date_sortie', $dateSortie, PDO::PARAM_STR);
-        $pdoStatement->bindValue(':date_rendu', $dateRendu, PDO::PARAM_STR);
-        $pdoStatement->execute();
-
-        $_SESSION['msgValidateAddEmprunt'] = "L'emprunt a été enregistré";
-
-        header('location: index.php');
-        exit;
-    }
-}
-
 require_once('include/_header.php');
 ?>
 <h1 class="text-center my-4">Bibliothèque | Emprunts</h1>
 
 <p class="text-center text-success fw-bold">
     <?php
-    if (isset($_SESSION['msgValidateAddEmprunt'])) echo $_SESSION['msgValidateAddEmprunt'];
-    unset($_SESSION['msgValidateAddEmprunt']); ?>
+    if (isset($_SESSION['msgValidate'])) echo $_SESSION['msgValidate'];
+    unset($_SESSION['msgValidate']); ?>
 </p>
 
 <div class="mx-auto">
@@ -94,28 +152,28 @@ require_once('include/_header.php');
             </tr>
         </thead>
         <tbody>
-            <?php foreach($data as $array): ?>
-            <tr>
-                <td><?=  $array['id_emprunt'] ?></td>
-                <td><?=  $array['auteur'] ?></td>
-                <td><?=  $array['titre'] ?></td>
-                <td><?=  $array['prenom'] ?></td>
-                <td><?=  $array['date_sortie'] ?></td>
-                <td>
-                <?php 
-                if(empty($array['date_rendu'])): ?>
-                    <span class='badge bg-warning'>Non rendu</span>
-                <?php 
-                else: 
-                    echo $array['date_rendu'];
-                endif
-                ?>
-                </td>
-                <td class="d-flex gap-2">
-                    <a href="?action=update&id=<?= $array['id_emprunt'] ?>" class="btn btn-primary">Modifier</a>
-                    <a href="?action=delete&id=<?= $array['id_emprunt'] ?>" class="btn btn-danger">Supprimer</a>
-                </td>
-            </tr>
+            <?php foreach ($data as $array): ?>
+                <tr>
+                    <td><?= $array['id_emprunt'] ?></td>
+                    <td><?= $array['auteur'] ?></td>
+                    <td><?= $array['titre'] ?></td>
+                    <td><?= $array['prenom'] ?></td>
+                    <td><?= $array['date_sortie'] ?></td>
+                    <td>
+                        <?php
+                        if (empty($array['date_rendu'])): ?>
+                            <span class='badge bg-warning'>Non rendu</span>
+                        <?php
+                        else:
+                            echo $array['date_rendu'];
+                        endif
+                        ?>
+                    </td>
+                    <td class="d-flex gap-2">
+                        <a href="?action=update&id=<?= $array['id_emprunt'] ?>" class="btn btn-primary">Modifier</a>
+                        <a href="?action=delete&id=<?= $array['id_emprunt'] ?>" class="btn btn-danger">Supprimer</a>
+                    </td>
+                </tr>
             <?php endforeach; ?>
         </tbody>
     </table>
@@ -123,32 +181,38 @@ require_once('include/_header.php');
     <form action="" method="post" class="mt-5">
         <div class="mb-3">
             <!-- <label for="abonne" class="form-label">Séléctionner un abonné</label> -->
-            <select class="form-select <?php if(isset($msgErrorIdAbonne)) echo $borderDanger ?>" id="abonne" name="abonne">
-                <option value="" selected>Séléctionner un abonné</option>
-                <?php foreach($dataSelectFormAbonne as $array): ?>
-                    <option value="<?= $array['id_abonne'] ?>"><?= $array['id_abonne'] ?> - <?= $array['prenom'] ?></option>
+            <select class="form-select <?php if (isset($msgErrorIdAbonne)) echo $borderDanger ?>" id="abonne" name="abonne">
+
+                <option value="">Séléctionner un abonné</option>
+
+                <?php foreach ($dataSelectFormAbonne as $array): ?>
+
+                    <option value="<?= $array['id_abonne'] ?>" <?php if (isset($arrayUpdateEmprunt['abonne_id']) && $arrayUpdateEmprunt['abonne_id'] === $array['id_abonne']) echo 'selected'; ?>><?= $array['id_abonne'] ?> - <?= $array['prenom'] ?></option>
+
                 <?php endforeach; ?>
             </select>
-            <small class="text-danger"><?php if(isset($msgErrorIdAbonne)) echo $msgErrorIdAbonne ?></small>
+            <small class="text-danger"><?php if (isset($msgErrorIdAbonne)) echo $msgErrorIdAbonne ?></small>
         </div>
         <div class="mb-3">
             <!-- <label for="livre" class="form-label">Séléctionner un livre</label> -->
-            <select class="form-select <?php if(isset($msgErrorIdLivre)) echo $borderDanger ?>" id="livre" name="livre">
-                <option value="" selected>Séléctionner un livre</option>
-                <?php foreach($dataSelectFormLivre as $array): ?>
-                    <option value="<?= $array['id_livre'] ?>"><?= $array['id_livre'] ?> - <?= $array['auteur'] ?> - <?= $array['titre'] ?></option>
+            <select class="form-select <?php if (isset($msgErrorIdLivre)) echo $borderDanger ?>" id="livre" name="livre">
+
+                <option value="">Séléctionner un livre</option>
+
+                <?php foreach ($dataSelectFormLivre as $array): ?>
+                    <option value="<?= $array['id_livre'] ?>" <?php if (isset($arrayUpdateEmprunt['livre_id']) && $arrayUpdateEmprunt['livre_id'] === $array['id_livre']) echo 'selected'; ?>><?= $array['id_livre'] ?> - <?= $array['auteur'] ?> - <?= $array['titre'] ?></option>
                 <?php endforeach; ?>
             </select>
-            <small class="text-danger"><?php if(isset($msgErrorIdLivre)) echo $msgErrorIdLivre ?></small>
+            <small class="text-danger"><?php if (isset($msgErrorIdLivre)) echo $msgErrorIdLivre ?></small>
         </div>
         <div class="mb-3">
             <label for="date_sortie" class="form-label">Date sortie</label>
-            <input type="date" class="form-control <?php if(isset($msgErrorDateSortie)) echo $borderDanger ?>" id="date_sortie" name="date_sortie">
-            <small class="text-danger"><?php if(isset($msgErrorDateSortie)) echo $msgErrorDateSortie ?></small>
+            <input type="date" class="form-control <?php if (isset($msgErrorDateSortie)) echo $borderDanger ?>" id="date_sortie" name="date_sortie" value="<?php if (isset($arrayUpdateEmprunt['date_sortie'])) echo $arrayUpdateEmprunt['date_sortie'] ?>">
+            <small class="text-danger"><?php if (isset($msgErrorDateSortie)) echo $msgErrorDateSortie ?></small>
         </div>
         <div class="mb-3">
             <label for="date_rendu" class="form-label">Date retour</label>
-            <input type="date" class="form-control" id="date_rendu" name="date_rendu">
+            <input type="date" class="form-control" id="date_rendu" name="date_rendu" value="<?php if (isset($arrayUpdateEmprunt['date_rendu']) && $arrayUpdateEmprunt['date_rendu'] !== null) echo $arrayUpdateEmprunt['date_rendu'] ?>">
         </div>
         <input type="submit" value="Enregistrer" class="btn btn-dark">
     </form>
--- livre.php
+++ livre.php
@@ -1,33 +1,144 @@
 <?php
+require_once('include/_init.php');
+
+if (isset($_GET['action']) && $_GET['action'] === 'delete') {
+    if (isset($_GET['id']) && !empty($_GET['id'])) {
+        $id = $_GET['id'];
+
+        // On selectionne tout dans la table emprunt en fonction de l'id de l'abonne dans l'url, celui que l'on souhaite supprimé
+        $data = $connect_db->prepare("SELECT * FROM emprunt WHERE livre_id = :livre_id");
+        $data->bindValue(':livre_id', $id, PDO::PARAM_INT);
+        $data->execute();
+
+        // Si le requete de sélection ne retourne aucun résultat, cela veut dire que l'abonné n'a aucun emprunt, alors on peut le supprimé
+        if (!$data->rowCount()) {
+            $data = $connect_db->prepare("DELETE FROM livgre WHERE id_livre = :id_livre");
+            $data->bindValue(':id_livre', $id, PDO::PARAM_INT);
+            $data->execute();
+
+            $_SESSION['messageValidation'] = "Le livre a été supprimé";
+        } else {
+            // Sinon l'abonné a des emprunts dans la bibliothèque, on affiche un message d'erreur
+            $_SESSION['errorDeleteMessage'] = "Impossible de supprimer le livre. Des emprunts y sont associés";
+        }
+
+        header('location: livre.php');
+        exit;
+    }
+}
+
+if (isset($_GET['action']) && $_GET['action'] === 'update') {
+    if (isset($_GET['id']) && !empty($_GET['id'])) {
+        $id = $_GET['id'];
+        $data = $connect_db->prepare("SELECT * FROM livre WHERE id_livre = :id_livre");
+        $data->bindValue(':id_livre', $id, PDO::PARAM_INT);
+        $data->execute();
+
+        // echo "Nombre de résultat : " . $data->rowCount();
+        if (!$data->rowCount()) {
+            header('location: livre.php');
+            exit;
+        }
+
+        $arrayUpdateLivre = $data->fetch(PDO::FETCH_ASSOC);
+        // echo '<pre>';
+        // print_r($arrayUpdateAbonne);
+        // echo '</pre>';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $auteur = $_POST['auteur'];
+            $titre = $_POST['titre'];
+            $pdoStatement = $connect_db->prepare("UPDATE livre SET auteur = :auteur, titre = :titre WHERE id_livre = :id_livre");
+            $pdoStatement->bindValue(':auteur', $auteur, PDO::PARAM_STR);
+            $pdoStatement->bindValue(':titre', $titre, PDO::PARAM_STR);
+            $pdoStatement->bindValue(':id_livre', $id, PDO::PARAM_INT);
+            $pdoStatement->execute();
+
+            $_SESSION['messageValidation'] = "Le livre a été modifié";
+
+            header('location: livre.php');
+            exit;
+        }
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $borderDanger = 'border border-danger';
+
+    $auteur = $_POST['auteur'];
+    $titre = $_POST['titre'];
+    if (empty($auteur)) {
+        $error = true;
+        $msgErrorAuteur = "Merci de saisir un auteur";
+    }
+
+    if (empty($titre)) {
+        $error = true;
+        $msgErrorTitre = "Merci de saisir un titre";
+    }
+
+    if (!isset($error)) {
+        $pdoStatement = $connect_db->prepare("INSERT INTO livre (auteur, titre) VALUE (:auteur, :titre)");
+        $pdoStatement->bindValue(':auteur', $auteur, PDO::PARAM_STR);
+        $pdoStatement->bindValue(':titre', $titre, PDO::PARAM_STR);
+        $pdoStatement->execute();
+
+        $_SESSION['messageValidation'] = "Le livre a été enregistré";
+
+        header('location: livre.php');
+        exit;
+    }
+}
+
+$pdoStatement = $connect_db->query("SELECT * FROM livre");
+$data = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+
 require_once('include/_header.php');
 ?>
 <h1 class="text-center my-4">Bibliothèque | Livres</h1>
 
+<p class="text-center text-success fw-bold">
+    <?php
+    if (isset($_SESSION['messageValidation'])) echo $_SESSION['messageValidation'];
+    unset($_SESSION['messageValidation']); ?>
+</p>
+
+<p class="text-center text-danger fw-bold">
+    <?php
+    if (isset($_SESSION['errorDeleteMessage'])) echo $_SESSION['errorDeleteMessage'];
+    unset($_SESSION['errorDeleteMessage']); ?>
+</p>
+
 <div class="col-md-8 mx-auto">
     <table class="table table-bordered mb-5 align-middle">
-        <tr>
-            <th>Auteur</th>
-            <th>Titre</th>
-            <th class="text-end">Actions</th>
-        </tr>
-        <tr>
-            <td>Lorem</td>
-            <td>Lorem</td>
-            <td class="text-end">
-                <a href="" class="btn btn-primary">Modifier</a>
-                <a href="" class="btn btn-danger">Supprimer</a>
-            </td>
-        </tr>
+        <thead>
+            <tr>
+                <th>Auteur</th>
+                <th>Titre</th>
+                <th class="text-end">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($data as $array): ?>
+            <tr>
+                <td><?= $array['auteur'] ?></td>
+                <td><?= $array['titre'] ?></td>
+                <td class="text-end">
+                    <a href="?action=update&id=<?= $array['id_livre'] ?>" class="btn btn-primary">Modifier</a>
+                    <a href="?action=delete&id=<?= $array['id_livre'] ?>" class="btn btn-danger">Supprimer</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
     </table>
 
     <form action="" method="post">
         <div class="mb-3">
             <label for="auteur" class="form-label">Auteur</label>
-            <input type="text" class="form-control" id="auteur" name="auteur" placeholder="Saisir un auteur">
+            <input type="text" class="form-control <?php if (isset($msgErrorAuteur)) echo $borderDanger; ?>" id="auteur" name="auteur" placeholder="Saisir un auteur" value="<?php if (isset($arrayUpdateLivre['auteur'])) echo $arrayUpdateLivre['auteur']; ?>">
         </div>
         <div class="mb-3">
             <label for="titre" class="form-label">Titre</label>
-            <input type="text" class="form-control" id="titre" name="titre" placeholder="Saisir un titre">
+            <input type="text" class="form-control <?php if (isset($msgErrorTitre)) echo $borderDanger; ?>" id="titre" name="titre" placeholder="Saisir un titre" value="<?php if (isset($arrayUpdateLivre['titre'])) echo $arrayUpdateLivre['titre']; ?>">
         </div>
         <input type="submit" value="Enregistrer" class="btn btn-dark">
     </form>
