1317138b206d04a6fd1695c58ce4504995e9d042
GregoryLacroix <gregorylacroix78@gmail.com>
2025-12-19 10:47:26 +0100

    develop abonne

--- abonne.php
+++ abonne.php
@@ -1,4 +1,24 @@
 <?php
+require_once('include/_init.php');
+
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $message = '';
+
+    $prenom = $_POST['prenom'];
+    if(!empty($prenom)){
+        // requete insertion
+    }else{
+        $message = "Merci de saisir un prénom";
+    }
+}
+
+$pdoStatement = $connect_db->query("SELECT * FROM abonne");
+$data = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+
 require_once('include/_header.php');
 ?>
 <h1 class="text-center my-4">Bibliothèque | Abonnés</h1>
@@ -9,19 +29,22 @@ require_once('include/_header.php');
             <th>Prénom</th>
             <th class="text-end">Actions</th>
         </tr>
+        <?php foreach($data as $array): ?>
         <tr>
-            <td>Lorem</td>
+            <td><?= $array['prenom'] ?></td>
             <td class="text-end">
-                <a href="" class="btn btn-primary">Modifier</a>
-                <a href="" class="btn btn-danger">Supprimer</a>
+                <a href="?action=update&id=<?= $array['id_abonne'] ?>" class="btn btn-primary">Modifier</a>
+                <a href="?action=delete&id=<?= $array['id_abonne'] ?>" class="btn btn-danger">Supprimer</a>
             </td>
         </tr>
+        <?php endforeach; ?>
     </table>
 
     <form action="" method="post">
         <div class="mb-3">
             <label for="prenom" class="form-label">Prénom</label>
             <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Saisir un prénom">
+            <small class="text-danger"><?php if(isset($message)) echo $message; ?></small>
         </div>
         <input type="submit" value="Enregistrer" class="btn btn-dark">
     </form>
---
+++ include/_init.php
@@ -0,0 +1,17 @@
+<?php
+// Connexion à la base de données
+$connect_db = new PDO('mysql:host=localhost;dbname=bibliotheque', 'user_entreprise', 'Entreprise28!', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+]);
+
+// Failles XSS
+foreach($_POST as $key => $value){
+    // $_POST['prenom'] = htmlspecialchars('<script>alert()</script>', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8')
+    $_POST[$key] = htmlspecialchars($_POST[$key], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+}
+
+foreach($_GET as $key => $value){
+    $_GET[$key] = htmlspecialchars($_GET[$key], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+}
+
